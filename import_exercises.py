ae824d2d227fb8b353b4b01629f21ed62ae954e5
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-07-12 17:00:50 -0500

    i hate this exercise

--- import_exercises.py
+++ import_exercises.py
@@ -27,21 +27,22 @@ letters = "abcd"
 result5 = list(permutations(letters, 2))
 print(len(result5))
 
-result6 = json.load(open('profiles.json'))
-print(result6)
 
-data = [
-    {"name": "John Doe", "isActive": True, "balance": "$1,234.56", "favoriteFruit": "apple", "unreadMessages": 3},
-    {"name": "Jane Smith", "isActive": False, "balance": "$2,345.67", "favoriteFruit": "banana", "unreadMessages": 0},
-    # Add more user data dictionaries here...
-]
+
+
+
+
+
+
+
+result6 = json.load(open('profiles.json'))
 
 # Total number of users
-total_users = len(data)
+total_users = len(result6)
 print("Total number of users:", total_users)
 
 # Number of active users
-active_users = sum(user["isActive"] for user in data)
+active_users = sum(user["isActive"] for user in result6)
 print("Number of active users:", active_users)
 
 # Number of inactive users
@@ -49,7 +50,7 @@ inactive_users = total_users - active_users
 print("Number of inactive users:", inactive_users)
 
 # Grand total of balances for all users
-total_balance = sum(float(user["balance"].replace("$", "").replace(",", "")) for user in data)
+total_balance = sum(float(user["balance"].replace("$", "").replace(",", "")) for user in result6)
 print("Grand total of balances for all users:", total_balance)
 
 # Average balance per user
@@ -57,15 +58,15 @@ average_balance = total_balance / total_users
 print("Average balance per user:", average_balance)
 
 # User with the lowest balance
-lowest_balance_user = min(data, key=lambda user: float(user["balance"].replace("$", "").replace(",", "")))
+lowest_balance_user = min(result6, key=lambda user: float(user["balance"].replace("$", "").replace(",", "")))
 print("User with the lowest balance:", lowest_balance_user["name"])
 
 # User with the highest balance
-highest_balance_user = max(data, key=lambda user: float(user["balance"].replace("$", "").replace(",", "")))
+highest_balance_user = max(result6, key=lambda user: float(user["balance"].replace("$", "").replace(",", "")))
 print("User with the highest balance:", highest_balance_user["name"])
 
-# Most common favorite fruit
-favorite_fruits = [user["favoriteFruit"] for user in data]
+# Most common favorite fruit, 
+favorite_fruits = [user["favoriteFruit"] for user in result6]
 most_common_fruit = max(set(favorite_fruits), key=favorite_fruits.count)
 print("Most common favorite fruit:", most_common_fruit)
 
@@ -74,5 +75,28 @@ least_common_fruit = min(set(favorite_fruits), key=favorite_fruits.count)
 print("Least common favorite fruit:", least_common_fruit)
 
 # Total number of unread messages for all users
-total_unread_messages = sum(user["unreadMessages"] for user in data)
+total_unread_messages = sum(user["unreadMessages"] for user in result6)
 print("Total number of unread messages for all users:", total_unread_messages)
+
+total_unread_messages = sum(int(user["greeting"].split()[10]) for user in result6)
+print("Total number of unread messages:", total_unread_messages)
+
+
+
+#trial
+
+"greeting": "Hello, Hebert Estes! You have 4 unread messages.",
+"greeting": "Hello, Allison Wynn! You have 19 unread messages.",
+
+unread_messages_count = 0
+
+for greeting in greetings:
+    # Extract the number of unread messages from the greeting
+    start_index = greeting.find('unread messages.') - 3
+    end_index = greeting.find('unread messages.')
+    unread_count = int(greeting[start_index:end_index])
+
+    # Add the unread count to the total count
+    unread_messages_count += unread_count
+
+print(f'Total unread messages: {unread_messages_count}')
