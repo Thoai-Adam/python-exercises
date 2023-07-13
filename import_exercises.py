08abf0d1ddecd64d22c2ad23254794f3392d8933
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-07-13 10:12:02 -0500

    completed import exercises

--- import_exercises.py
+++ import_exercises.py
@@ -53,13 +53,32 @@ print("Number of inactive users:", inactive_users)
 total_balance = sum(float(user["balance"].replace("$", "").replace(",", "")) for user in result6)
 print("Grand total of balances for all users:", total_balance)
 
+"""total_balance = 10000  # Example total balance
+total_users = 50  # Example total number of users
+
+average_balance = total_balance / total_users
+rounded_average_balance = round(average_balance, 2)
+
+print("Average balance per user:", rounded_average_balance)"""
+
 # Average balance per user
 average_balance = total_balance / total_users
-print("Average balance per user:", average_balance)
+print("Average balance per user:", (average_balance))
 
 # User with the lowest balance
 lowest_balance_user = min(result6, key=lambda user: float(user["balance"].replace("$", "").replace(",", "")))
 print("User with the lowest balance:", lowest_balance_user["name"])
+
+#could be written as
+"""balance_list = []
+for profile in profiles:
+        profile_balance = float(profile['balance'].strip('$').replace(',',''))
+        balance_list.append(profile_balance)
+min(balance_list)
+for profile in profiles:
+        if float(profile['balance'].strip('$').replace(',','')) == min(balance_list):
+                print(profile['name'], min(balance_list))"""
+
 
 # User with the highest balance
 highest_balance_user = max(result6, key=lambda user: float(user["balance"].replace("$", "").replace(",", "")))
@@ -74,29 +93,12 @@ print("Most common favorite fruit:", most_common_fruit)
 least_common_fruit = min(set(favorite_fruits), key=favorite_fruits.count)
 print("Least common favorite fruit:", least_common_fruit)
 
-# Total number of unread messages for all users
-total_unread_messages = sum(user["unreadMessages"] for user in result6)
-print("Total number of unread messages for all users:", total_unread_messages)
+total_messages = 0
+for profile in result6:
+    message = profile['greeting'].split(' ')
+    for word in message: 
+        if word.isdigit():
+            total_messages += int(word)
+print('The number of unread messages are',total_messages)
 
-total_unread_messages = sum(int(user["greeting"].split()[10]) for user in result6)
-print("Total number of unread messages:", total_unread_messages)
 
-
-
-#trial
-
-"greeting": "Hello, Hebert Estes! You have 4 unread messages.",
-"greeting": "Hello, Allison Wynn! You have 19 unread messages.",
-
-unread_messages_count = 0
-
-for greeting in greetings:
-    # Extract the number of unread messages from the greeting
-    start_index = greeting.find('unread messages.') - 3
-    end_index = greeting.find('unread messages.')
-    unread_count = int(greeting[start_index:end_index])
-
-    # Add the unread count to the total count
-    unread_messages_count += unread_count
-
-print(f'Total unread messages: {unread_messages_count}')
